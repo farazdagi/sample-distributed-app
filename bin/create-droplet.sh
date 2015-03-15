585a7dcf199e813aba712b38aa1fc2a00cdb3610
Victor Farazdagi <simple.square@gmail.com>
2015-03-15 22:56:39 +0300

    Trivial node creating config and script

---
+++ bin/create-droplet.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Note: make sure that SSH_KEY_ID is SSH **ID**, not name,
+# to discover key's ID see: https://developers.digitalocean.com/documentation/v2/#list-all-keys
+
+curl -X POST https://api.digitalocean.com/v2/droplets \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $TOKEN" \
+    -d '
+{
+    "name":"core1",
+    "region":"ams3",
+    "size":"512mb",
+    "image":"coreos-stable",
+    "ssh_keys":['$SSH_KEY_ID'],
+    "backups":false,
+    "private_networking":true,
+    "user_data": "'"$(cat config/cloud-config.yaml | sed 's/"/\\"/g')"'"
+}'
